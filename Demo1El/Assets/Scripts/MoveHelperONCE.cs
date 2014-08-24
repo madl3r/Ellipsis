4c89760d0c0d5164c16e4819aaa9ae0b80994ec7
Matthew Adler <madler72@gmail.com>
2014-08-24 15:23:34 -0400

    Fixed switching bug (now works as intended) Cleaned up code and added more descriptive comments.

---
+++ Demo1El/Assets/Scripts/MoveHelperONCE.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveHelperONCE : MonoBehaviour {
+
+	//TODO NOTE!!
+	//It is very important that this script is executed AFTER the "Movement" script.
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//the current first goes based on the original starting pos in the Q of the dot
+		//this is sorta like an ID for it.
+	
+		//NOTICE that the addition and subtraction is opposite of that in the switchR and L methods of Movement.
+		//That's because when you move 1 to 0 in movement you need to subtract. But here we had the orig at 0, and now the ID must be 1... so we add!
+		if (Input.GetKeyDown("right"))
+		{
+			//when switching right we want the next in line to be the new leader
+			Movement.currentFirst = (Movement.currentFirst + 1) % 3;
+		}
+		if (Input.GetKeyDown("left"))
+		{
+			//when switching left we want the prev in line to be the new leader.
+			Movement.currentFirst = (Movement.currentFirst + 2) % 3;
+		}
+
+	}
+}
--- Demo1El/Assets/Scripts/Movement.cs
+++ Demo1El/Assets/Scripts/Movement.cs
@@ -4,74 +4,91 @@ using System.Collections.Generic;
 
 public class Movement : MonoBehaviour {
 
-	private float theYPos;
-	private bool leadIsFar;
+	//To avoid multiple activations of switches which would occur based on arbitrary exec order of dots movement scripts.
+	public static int currentFirst = 0;
 
-	public float numInQ;
+	//current position in the Q
+	public int numInQ;
+	//handles to the next and prev objects in line.
 	public GameObject nextInQ;
+	public GameObject prevInQ;
+	//the name
+	public string thisName;
 
+	
+
+	//The position that it will follow towards.
+	private float theYPos;
+	//checks if the next in line is far enough away that it should start to follow.
+	private bool leadIsFar;
+	//The original position in the Q
+	//This variable shouldn't change past start and acts as a sort of ID for the dot.
+	private int origNumInQ;
+
+	//to be used with the "World" script. the next line that this wants to go to
 	private GameObject lineTarget;
+	//index of the lineTarget
 	private int lineTargetIndex;
+	//handle to the empty GameObject with the "World" script on it
 	private GameObject theWorld;
+
+	//temp vars for switching
+	public Vector2 posOnSwitch;
+	public float theYPosOnSwitch;
 
 	// Use this for initialization
 	void Start () 
 	{
+		//getting this dots original position in the Q.
+		origNumInQ = numInQ;
+		//theYPost defaults to 0
 		theYPos = 0.0f;
+		//getting that empty GameObject
 		theWorld = GameObject.FindGameObjectWithTag("theWorld");
 
-		theWorld.SendMessage("printMessage");
+		//DebugCode
+		//theWorld.SendMessage("printMessage");
 
+		//starting the lineTarget to be the line that we're currently on.
 		foreach (GameObject line in theWorld.GetComponent<World>().lines)
 		{
 			if (line.transform.position.y  == theYPos)
 				lineTarget = line;
 		}
-
+		//now getting it's index.
 		lineTargetIndex = theWorld.GetComponent<World>().lines.IndexOf(lineTarget);
-
-		Debug.Log("the lineTarget's position" + lineTarget.transform.position + "With Index at: " + lineTargetIndex);
+		//DebugCode
+		//Debug.Log("the lineTarget's position" + lineTarget.transform.position + "With Index at: " + lineTargetIndex);
 
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		//Y MOVEMENT
-		//~~~~~~~WITH this if taken out things will continue to move to the correct line even when order
-			// is switched mid move. THERE IS A JUMP THOUGH THAT IS DUE TO KEEPING THE SAME Y VALUE DURING A SWITCH
-			// THIS JUMP MIGHT BE FIXED THROUGH THE EVENTUAL LERPING... OR BY MAKING THE Y VALUE EQUAL TO WHOEVER IS INFRONT OF OR BEHIND DEPENDING ON THE SWITCH
-
-			//Maybe don't physically move them... instead on this movement just change the stats of each one including color. That might be weird with order of things...
-				//Probably better to just find out how to smooth out the jumping
-			//ACTUALLY PROBABLY DON'T MOVE THEM! WHENEVER DOING A SWITCH (L or R) send messages to each one so you know the order that it will happen in!!
-				//Don't do a move within the game, instead do an animation to black and then change colors.
-
-		//if (numInLine == 0)
-		//{
 			//If first in line update this one's theYPos to the next line
 			//In the future it will NOT be a hard coded value. Instead will go to the next line up or down!!
 
 			//TODO Should do an axis for controllers... but for now will just do getKeyDown
 
 			//instead of manually changing up or down should get the next line up or down in the list of lines from world.
-				//if it's allowed to do that based on "World's" decision then set theYPos to that lines y pos.
-			if (Input.GetKeyDown("up") && theYPos < 4.0f)
-			{
-				theYPos += 2f;
-			}
+			//if it's allowed to do that based on "World's" decision then set theYPos to that lines y pos.
 
-			if (Input.GetKeyDown("down") && theYPos > -4.0f)
-			{
-				theYPos -= 2f;
-			}
+		//Up or down, then move theYPos for errone
+		if (Input.GetKeyDown("up") && theYPos < 4.0f)
+		{
+			theYPos += 2f;
+		}
 
-			//Locking onto a line
-			if(Mathf.Abs(transform.position.y - theYPos) < 0.05f)
-				transform.position = new Vector2 (transform.position.x, theYPos);
-		//}
-		//Else slowly follow the person in front of you.
-		//else
+		if (Input.GetKeyDown("down") && theYPos > -4.0f)
+		{
+			theYPos -= 2f;
+		}
+
+		//Locking onto a line
+		if(Mathf.Abs(transform.position.y - theYPos) < 0.05f)
+		transform.position = new Vector2 (transform.position.x, theYPos);
+
+		//If you're not first in line then you want to pay attention to how far the next in line is from you.
 		if (numInQ != 0)
 		{
 			if (Mathf.Abs(transform.position.y - nextInQ.transform.position.y) > 0.5f)
@@ -83,48 +100,33 @@ public class Movement : MonoBehaviour {
 				leadIsFar = false;
 				//clicking onto the line and updating theYPos
 				transform.position = new Vector2 (transform.position.x, nextInQ.transform.position.y);
-				//theYPos = transform.position.y;
+				//theYPos = transform.position.y; DON'T NEED THIS ANYMORE B/C THEYPOS WILL ALWAYS BE UPDATED
 			}
 		}
 
+//		//Switching right
+//		if (Input.GetKeyDown("right"))
+//		{
+//			Debug.Log("GETKEYDOWN TOO MUCH. Current First is: " + currentFirst);
+//		}
+//		//switchting left
+//		if (Input.GetKeyDown("left"))
+//		{
+//			Debug.Log("GETKEYDOWN TOO MUCH ~~~~~~ LLL " + currentFirst);
+//		}
 
-		//Input detects movement right.
-		if (Input.GetKeyDown("right"))
+		//Switching right
+		//If we get right input, only do on the first in line, and make sure that this is only the first time detected (SEE MoveHelperONCE)
+		if (Input.GetKeyDown("right") && numInQ == 0 && origNumInQ == currentFirst)
 		{
-			//first go to back
-			if (numInQ == 0)
-			{
-				//Move to back
-				transform.position = new Vector2 (transform.position.x - 2, transform.position.y);
-				numInQ = 2;
-				//now have a next in line... get it :/ nvm the order never changes
-				
-			}
-			//second or third move up one
-			else if (numInQ != 0)
-			{
-				//third keeps the same in front of it
-				transform.position = new Vector2 (transform.position.x + 1, transform.position.y);
-				numInQ -= 1;
-			}
-			
+			//Debug.Log("GETKEYDOWN TOO MUCH. Current First is: " + currentFirst);
+			switchRight(0);
 		}
-		//Input movement left
-		if (Input.GetKeyDown("left"))
+		//Same as above, but for switching left
+		if (Input.GetKeyDown("left") && numInQ == 0 && origNumInQ == currentFirst)
 		{
-			//move back one
-			if ((numInQ == 0) || (numInQ == 1))
-			{
-				transform.position = new Vector2 (transform.position.x - 1, transform.position.y);
-				numInQ += 1;
-			}
-			//last move to the front
-			else if (numInQ == 2)
-			{
-				transform.position = new Vector2 (transform.position.x + 2, transform.position.y);
-				numInQ = 0;
-			}
-			
+			//Debug.Log("GETKEYDOWN TOO MUCH ~~~~~~ LLL " + currentFirst);
+			switchLeft(0);
 		}
 
 	}
@@ -139,13 +141,108 @@ public class Movement : MonoBehaviour {
 			else if (transform.position.y > theYPos)
 				transform.position = new Vector2 (transform.position.x, transform.position.y - 0.2f);
 		}
-		//later in line following movement.
+		//Not first in line following movement.
 		else
 		{
 			if (leadIsFar && transform.position.y < nextInQ.transform.position.y)
 				transform.position = new Vector2 (transform.position.x, transform.position.y + 0.2f);
 			else if (leadIsFar && transform.position.y > nextInQ.transform.position.y)
 				transform.position = new Vector2 (transform.position.x, transform.position.y - 0.2f);
+		}
+	}
+
+	void switchRight (int attempt)
+	{
+
+		if (attempt > 2)
+		{
+			//set the temp position to null.
+			//Debug.Log("~~~~~~~~~~~~~~~~~~~~~~~~~~");
+			return;
+		}
+		else
+		{
+			//Debug.Log(attempt + " done by: " + thisName);
+
+			//if (attempt == 0)
+			//store current position in the temp value
+			//Also store theYPos
+			if (attempt == 0)
+			{
+				posOnSwitch = new Vector2 (transform.position.x, transform.position.y);
+				theYPosOnSwitch = theYPos;
+			}
+
+			//if attempt == 0, or 1 then move to next in lines position (also copy theYPos)
+			if (attempt == 0 || attempt == 1)
+			{
+				transform.position = new Vector2 (nextInQ.transform.position.x, nextInQ.transform.position.y);
+			}
+
+			//else if (attempt == 2) move to next in lines temp (because that one will have already have moved)
+			else if (attempt == 2)
+			{
+				transform.position = new Vector2 (nextInQ.GetComponent<Movement>().posOnSwitch.x, nextInQ.GetComponent<Movement>().posOnSwitch.y);
+				theYPos = nextInQ.GetComponent<Movement>().theYPosOnSwitch;
+			}
+
+			//No matter who it is. When we move right we want to decrease the position in Q for each dot.
+			// Note that  +2%3 is equivalent to -1%3... but unity does mods in a way that (-1)%3 = -1 instead of 2.
+			//Hence doing +2 instead of -1
+			numInQ = (numInQ + 2) % 3;
+
+			//Always incremenet the attempt because no matter what we will move onto the next dot.
+			attempt++;
+
+			//Send message (calling this method) to next in Q
+			nextInQ.SendMessage("switchRight", attempt);
+		}
+	}
+
+	void switchLeft (int attempt)
+	{
+		//Same as above but negative
+		
+		if (attempt > 2)
+		{
+			//set the temp position to null.
+			//Debug.Log("~~~~~~~~~~~~~~~~~~~~~~~~~~");
+			return;
+		}
+		else
+		{
+			//Debug.Log(attempt + " LEFT done by: " + thisName);
+			
+			//if (attempt == 0)
+			//store current position in the temp value
+			//Also store theYPos
+			if (attempt == 0)
+			{
+				posOnSwitch = new Vector2 (transform.position.x, transform.position.y);
+				theYPosOnSwitch = theYPos;
+			}
+			
+			//if attempt == 0, or 1 then move to next in lines position (also copy theYPos)
+			if (attempt == 0 || attempt == 1)
+			{
+				transform.position = new Vector2 (prevInQ.transform.position.x, prevInQ.transform.position.y);
+			}
+			
+			//else if (attempt == 2) move to next in lines temp
+			else if (attempt == 2)
+			{
+				transform.position = new Vector2 (prevInQ.GetComponent<Movement>().posOnSwitch.x, prevInQ.GetComponent<Movement>().posOnSwitch.y);
+				theYPos = prevInQ.GetComponent<Movement>().theYPosOnSwitch;
+			}
+			
+			//No matter who it is, when going left we inscrease their pos in the Q by 1 (mod 3)
+			numInQ = (numInQ + 1) % 3;
+
+			//Always incremenet the attempt because no matter what we will move onto the next dot.
+			attempt++;
+			
+			//Send message (calling this method) to prev in Q
+			prevInQ.SendMessage("switchLeft", attempt);
 		}
 	}
 }
