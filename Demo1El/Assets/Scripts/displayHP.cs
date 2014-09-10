01515f482c9004ec06c62266ccb74e714316658d
Matthew Adler <madler72@gmail.com>
2014-09-10 13:55:33 -0400

    HP UI is functional. Game ends when player dies.

---
+++ Demo1El/Assets/Scripts/displayHP.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class displayHP : MonoBehaviour {
+
+	public GameObject heartSprite;
+	public GameObject[] hearts;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void showHearts(int hpAmt)
+	{
+		//Kill all the hearts that are there
+		for (int i = 0; i < hearts.Length; i++)
+		{
+			if (hearts[i] != null)
+				Destroy(hearts[i]);
+		}
+
+		//Pu the new hearts in
+		if (hpAmt > 0){
+			hearts = new GameObject[hpAmt];
+
+			for (int i = 0; i < hearts.Length; i++)
+			{
+				hearts[i] = Instantiate(heartSprite, new Vector2(transform.position.x + i, transform.position.y), transform.rotation) as GameObject;
+			}
+		}
+	}
+}
--- Demo1El/Assets/Scripts/playerStats.cs
+++ Demo1El/Assets/Scripts/playerStats.cs
@@ -6,16 +6,22 @@ public class playerStats : MonoBehaviour {
 	//This class keeps track of and updates the players stats (health and upgrades)
 		//This class is then also in charge of using the upgrades that are currently on it... for now.
 	private static int hp;
-	
+	public static GameObject HPUI;
+
 	//Attack type maybe for close range, shoot, and deflect... then add modifiers like spread and what not
 	public GameObject attackType;
+
 	private int bonusDmg;
 
 	//Should include attack damage, and attack speed here as well too.
 
 	// Use this for initialization
 	void Start () {
+		HPUI = GameObject.Find("HPui");
+
 		hp = 5;
+		//HPUI.GetComponent<displayHP>.hearts = new GameObject[hp];
+		HPUI.GetComponent<displayHP>().showHearts(hp);
 		bonusDmg = 0;
 	}
 	
@@ -28,9 +34,15 @@ public class playerStats : MonoBehaviour {
 	{
 		hp -= dmg;
 
+		HPUI.GetComponent<displayHP>().showHearts(hp);
+
 		if (hp <= 0)
 		{
 			Debug.Log ("HP is lower than 0 GG");
+			GameObject.Find("GameOverScreen").renderer.enabled = true;
+			Time.timeScale = 0;
+			//Pause the game and show a game over screen
+			//Run death animation
 		}
 
 	}
